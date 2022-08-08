7d263dd8178549eda2c7e929e94a825b91f21968
10946068 <103135172+10946068@users.noreply.github.com>
2022-08-08 11:05:18 +0100

    DCIT 104 (ASSIGNMENT 1)

---
+++ DCIT 104 (10946068) ZAKARIA SAFURATU/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int even[];
+        even = new int[10000];
+        int count = 0;
+        int sum = 0;
+
+
+        for (int i = 1;i < 10000; i++){
+
+            if(i % 2 == 0){
+                even[count] = i;
+                count++;
+            }
+        }
+
+        for (int i = 0;i < count; i++){
+            sum += even[i];
+        }
+
+        System.out.println("Average: " + sum/count);
+
+    }
+}
